5edef71a1f1f903229a90d055487f75092d3d766
stokes <40156487+djns99@users.noreply.github.com>
2024-12-22 22:37:42 +1300

    Day 15 pt 2

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,14 +1,65 @@
 use std::io;
 use std::mem::swap;
 
-fn try_step(grid: &mut Vec<Vec<char>>, pos: (usize, usize), dir: (isize, isize)) -> (usize, usize) {
+fn advance(pos: (usize, usize), dir: (isize, isize)) -> (usize, usize) {
     let newpos = (pos.0 as isize + dir.0, pos.1 as isize + dir.1);
-    let newpos = (newpos.0 as usize, newpos.1 as usize);
+    (newpos.0 as usize, newpos.1 as usize)
+}
+
+fn scan_dir(grid: &Vec<Vec<char>>, pos: (usize, usize), dir: (isize, isize)) -> bool {
+    let (mut x, mut y) = pos;
+    match grid[y][x] {
+        '#' => false,
+        '.' => true,
+        v => {
+            let is_left = v == '[';
+            if is_left {
+                x += 1;
+            }
+            scan_dir(grid, advance((x-1, y), dir), dir) && scan_dir(grid, advance((x, y), dir), dir)
+        }
+        _ => panic!(),
+    }
+}
+
+fn try_step_block(grid: &mut Vec<Vec<char>>, pos: (usize, usize), dir: (isize, isize)) -> (usize, usize) {
+    let (mut x, y) = pos;
+    if !scan_dir(grid, pos, dir) {
+        return pos
+    }
+
+    let is_left = grid[y][x] == '[';
+    if is_left {
+        x += 1;
+    }
+    let left_pos = (x - 1, y);
+    let right_pos = (x, y);
+    let left_step = try_step(grid, left_pos, dir);
+    let right_step = try_step(grid, right_pos, dir);
+    if is_left {
+        left_step
+    } else {
+        right_step
+    }
+}
+
+fn try_step(grid: &mut Vec<Vec<char>>, pos: (usize, usize), dir: (isize, isize)) -> (usize, usize) {
+    let newpos = advance(pos, dir);
     let (x, y) = newpos;
     // Try clear the way if we can
-    if grid[y][x] == 'O' {
-        try_step(grid, newpos, dir);
-    }
+    match grid[y][x] {
+        'O' => {
+            try_step(grid, newpos, dir);
+        }
+        '[' | ']' => {
+            if dir.1 == 0 {
+                try_step(grid, newpos, dir); // Moving sideways, just step normally
+            } else {
+                try_step_block(grid, newpos, dir); // Need to do a coupled step
+            }
+        }
+        _ => {}
+    };
     match grid[y][x] {
         '.' => {
             grid[y][x] = grid[pos.1][pos.0];
@@ -24,7 +75,7 @@ fn try_step(grid: &mut Vec<Vec<char>>, pos: (usize, usize), dir: (isize, isize))
 fn part1(mut grid: Vec<Vec<char>>, instructions: &Vec<char>) {
     let mut robot_pos = grid.iter()
         .enumerate()
-        .filter_map(|(y, row)| Some((y, row.iter().position(|&x| x == '@')?)))
+        .filter_map(|(y, row)| Some((row.iter().position(|&x| x == '@')?, y)))
         .next().unwrap();
     println!("Robot starting at {:?}", robot_pos);
 
@@ -37,6 +88,9 @@ fn part1(mut grid: Vec<Vec<char>>, instructions: &Vec<char>) {
             _ => (0, 0)
         };
 
+        // grid.iter().for_each(|row| {
+        //     println!("{:?}", row.iter().collect::<String>());
+        // });
         robot_pos = try_step(&mut grid, robot_pos, dir);
     }
     println!("Final position");
@@ -48,7 +102,7 @@ fn part1(mut grid: Vec<Vec<char>>, instructions: &Vec<char>) {
         .fold(0, |acc, (y, vec)| {
             vec.iter().enumerate()
                 .fold(acc,
-                      |a, (x, &c) | if c == 'O' { a + x + y * 100 } else { a }
+                      |a, (x, &c)| if c == 'O' || c == '[' { a + x + y * 100 } else { a },
                 )
         });
 
@@ -56,7 +110,18 @@ fn part1(mut grid: Vec<Vec<char>>, instructions: &Vec<char>) {
 }
 
 
-fn part2(grid: &Vec<Vec<char>>, instructions: &Vec<char>) {}
+fn part2(grid: &Vec<Vec<char>>, instructions: &Vec<char>) {
+    let grid: Vec<Vec<char>> = grid.into_iter().map(|x| x.into_iter().map(|&c| match c {
+        'O' => ['[', ']'],
+        '@' => ['@', '.'],
+        v => [v, v],
+    }).flatten().collect()).collect();
+    grid.iter().for_each(|row| {
+        println!("{:?}", row);
+    });
+
+    part1(grid, instructions);
+}
 
 fn main() {
     let lines: Vec<Vec<char>> = io::stdin()
